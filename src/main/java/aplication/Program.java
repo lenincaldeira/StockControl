eb818e4215b2be188b42d935fc0689ea38d7ad56
lenincaldeira <lenin.caldeira@gmail.com>
2022-08-10 22:47:32 -0300

    Aula 68 - Curso de JAVA da UDEMY

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -20,7 +20,7 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product.toString());
+        System.out.println(product);
 
         sc.close();
     }
--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -17,7 +17,14 @@ public class Product {
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
+
     public String toString() {
-        return name;
+        return name
+            + ", $"
+            + String.format("%.2f", price)
+            + ", "
+            + quantity
+            + " units, Total: $ "
+            + String.format("%.2f", totalValueInStock());
     }
 }
